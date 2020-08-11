ad0f66188259d13e024d9a59f21a650d26df7b21
YU LI <yu.li.sallylee@gmail.com>
2020-08-11 14:28:21 +0800

    Create train_preprocessing.py

---
+++ tiny-imagenet-200/train_preprocessing.py
@@ -0,0 +1,26 @@
+
+import io
+import pandas as pd
+import glob
+import os
+from shutil import move
+from os.path import join
+from os import listdir, rmdir
+
+target_folder = './train/'
+train_dict = {}
+
+classes = glob.glob(target_folder + '/*')
+print (classes)
+
+for class_path in classes:
+	paths = glob.glob(class_path + '/*')
+	for path in paths:
+		print (path)
+		if 'txt'in path:
+			os.remove(path)
+		else:
+			for f in os.listdir(path):
+				move(os.path.join(path, f), class_path)
+
+				
